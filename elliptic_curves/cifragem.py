c13857f1d8e981d2ae2d17b2c54536119140e236
lucastercas <lucasmtercas@gmail.com>
2019-11-18 03:52:54 -0300

    feat(cipher): Create basic structure of cipher file

--- elliptic_curves/cifragem.py
+++ elliptic_curves/cifragem.py
@@ -1,4 +1,37 @@
+#!/usr/bin/env python
+
+import random
+from utils import readFile, writeFile
+
+
+def calculateCurve(p, d, e):
+    pass
+
+
+def defineQ(curve):
+    pass
+
+
+def getPoints(plain_text):
+    pass
+
+
+def cipher(pm):
+    pass
+
+
 def main():
-  pass
+    p, d, e = 21, 1, 1
+    curve = calculateCurve(p, d, e)
+    writeFile("./curve.txt", curve)
+
+    q = defineQ(curve)
+    k = random()
+    plain_text = readFile("./textos/texto-claro.txt")
+    pm = getPoints(plain_text)
+
+    cipher_text = cipher(pm)
+    writeFile("./textos/texto-cifrado.txt", cipher_text)
+
 
 main()
